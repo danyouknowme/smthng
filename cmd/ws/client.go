dbedb8dfecad98607e8dcf9aa7abe52d0b6fe9f8
Thanathip Suwannakhot <thanathip.suw@gmail.com>
2023-07-03 15:55:44 +0700

    Added read and write pump in websocket client

--- cmd/ws/client.go
+++ cmd/ws/client.go
@@ -1,6 +1,33 @@
 package ws
 
-import "github.com/gorilla/websocket"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"time"
+
+	v1 "github.com/danyouknowme/synnox/internal/bussiness/domains/v1"
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
+)
+
+const (
+	WriteWait      = 10 * time.Second
+	PongWait       = 60 * time.Second
+	PingPeriod     = (PongWait * 9) / 10
+	MaxMessageSize = 10000
+)
+
+var (
+	newline  = []byte{'\n'}
+	upgrader = websocket.Upgrader{
+		ReadBufferSize:  4096,
+		WriteBufferSize: 4096,
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
+)
 
 type Client struct {
 	ID    string
@@ -8,4 +35,115 @@ type Client struct {
 	hub   *Hub
 	send  chan []byte
 	rooms map[*Room]bool
+}
+
+func newClient(conn *websocket.Conn, hub *Hub, id string) *Client {
+	return &Client{
+		ID:    id,
+		conn:  conn,
+		hub:   hub,
+		send:  make(chan []byte, 256),
+		rooms: make(map[*Room]bool),
+	}
+}
+
+func (client *Client) readPump() {
+	defer func() {
+		client.disconnect()
+	}()
+
+	client.conn.SetReadLimit(MaxMessageSize)
+
+	_ = client.conn.SetReadDeadline(time.Now().Add(PongWait))
+
+	client.conn.SetPongHandler(func(string) error {
+		_ = client.conn.SetReadDeadline(time.Now().Add(PongWait))
+		return nil
+	})
+
+	for {
+		_, jsonMessage, err := client.conn.ReadMessage()
+		if err != nil {
+			break
+		}
+		client.handleNewMessage(jsonMessage)
+	}
+}
+
+func (client *Client) writePump() {
+	ticker := time.NewTicker(PingPeriod)
+	defer func() {
+		ticker.Stop()
+		_ = client.conn.Close()
+	}()
+
+	for {
+		select {
+		case message, ok := <-client.send:
+			_ = client.conn.SetWriteDeadline(time.Now().Add(WriteWait))
+			if !ok {
+				// The hub closed the channel.
+				_ = client.conn.WriteMessage(websocket.CloseMessage, []byte{})
+				return
+			}
+
+			w, err := client.conn.NextWriter(websocket.TextMessage)
+			if err != nil {
+				return
+			}
+			_, _ = w.Write(message)
+
+			// Attach queued chat messages to the current websockets message.
+			n := len(client.send)
+			for i := 0; i < n; i++ {
+				_, _ = w.Write(newline)
+				_, _ = w.Write(<-client.send)
+			}
+
+			if err := w.Close(); err != nil {
+				return
+			}
+		case <-ticker.C:
+			_ = client.conn.SetWriteDeadline(time.Now().Add(WriteWait))
+			if err := client.conn.WriteMessage(websocket.PingMessage, nil); err != nil {
+				return
+			}
+		}
+	}
+}
+
+func (client *Client) disconnect() {
+	client.hub.unregister <- client
+
+	for room := range client.rooms {
+		room.unregister <- client
+	}
+	close(client.send)
+
+	_ = client.conn.Close()
+}
+
+func ServeWs(hub *Hub, ctx *gin.Context) {
+	userId := ctx.MustGet("userId").(string)
+
+	conn, err := upgrader.Upgrade(ctx.Writer, ctx.Request, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	client := newClient(conn, hub, userId)
+
+	go client.writePump()
+	go client.readPump()
+
+	hub.register <- client
+}
+
+func (client *Client) handleNewMessage(jsonMessage []byte) {
+
+	var message v1.ReceivedMessage
+	if err := json.Unmarshal(jsonMessage, &message); err != nil {
+		log.Printf("Error on unmarshal JSON message %s", err)
+	}
 }
