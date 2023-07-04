38f1cc5ef29e989f66c0b27e1440df31b5e8b524
Thanathip Suwannakhot <thanathip.suw@gmail.com>
2023-07-04 21:41:56 +0700

    Added function load config from .env file

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,11 +2,22 @@ package main
 
 import (
 	"github.com/danyouknowme/smthng/cmd/api/server"
+	"github.com/danyouknowme/smthng/internal/config"
 	"github.com/danyouknowme/smthng/pkg/logger"
 )
 
+var appConfig config.AppConfig
+var err error
+
+func init() {
+	err = config.Load(&appConfig)
+	if err != nil {
+		logger.Fatal(err)
+	}
+}
+
 func main() {
-	app := server.NewApp()
+	app := server.NewApp(&appConfig)
 	if err := app.Start(); err != nil {
 		logger.Fatal(err)
 	}
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,23 @@
 package config
+
+import "github.com/spf13/viper"
+
+type AppConfig struct {
+	Env  string
+	Port string
+}
+
+func Load(config *AppConfig) error {
+	viper.AddConfigPath(".")
+	viper.SetConfigName(".env")
+	viper.SetConfigType("env")
+	viper.AutomaticEnv()
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		return err
+	}
+
+	err = viper.Unmarshal(&config)
+	return err
+}
