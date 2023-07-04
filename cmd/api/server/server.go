680666dcbda87af060f42c07742414f5b5ff549b
Thanathip Suwannakhot <thanathip.suw@gmail.com>
2023-07-04 21:44:43 +0700

    Added config when create new app

--- cmd/api/server/server.go
+++ cmd/api/server/server.go
@@ -10,6 +10,7 @@ import (
 	"time"
 
 	"github.com/danyouknowme/smthng/cmd/ws"
+	"github.com/danyouknowme/smthng/internal/config"
 	"github.com/danyouknowme/smthng/internal/http/routes"
 	"github.com/danyouknowme/smthng/pkg/logger"
 	"github.com/gin-gonic/gin"
@@ -17,24 +18,26 @@ import (
 
 type App struct {
 	httpServer *http.Server
+	config     *config.AppConfig
 }
 
-func NewApp() *App {
+func NewApp(config *config.AppConfig) *App {
 	router := initRouter()
 
 	server := &http.Server{
-		Addr:    ":8000",
+		Addr:    ":" + config.Port,
 		Handler: router,
 	}
 
 	return &App{
 		httpServer: server,
+		config:     config,
 	}
 }
 
 func (a *App) Start() error {
 	go func() {
-		logger.Info("Server listening at port 8000")
+		logger.Infof("Server listening at port %s", a.config.Port)
 		logger.Info("Starting listening for HTTP requests completed")
 		if err := a.httpServer.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 			logger.Fatalf("Failed to listen and serve: %+v", err)
