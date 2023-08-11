24e3adaa55d5f1d49c84fbccffad1835e65be664
Thanathip Suwannakhot <thanathip.suw@gmail.com>
2023-08-11 23:39:47 +0700

    Fixed middleware response format

--- internal/http/middleware/middleware.go
+++ internal/http/middleware/middleware.go
@@ -1,11 +1,10 @@
 package middleware
 
 import (
-	"errors"
-	"fmt"
 	"net/http"
 	"strings"
 
+	"github.com/danyouknowme/smthng/pkg/apperrors"
 	"github.com/danyouknowme/smthng/pkg/jwt"
 	"github.com/gin-gonic/gin"
 )
@@ -21,42 +20,47 @@ func AuthMiddleware(jwtService jwt.JWTService) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		authorizationHeader := ctx.GetHeader(authorizationHeaderKey)
 		if len(authorizationHeader) == 0 {
-			err := errors.New("authorization header is not provided")
-			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
-				"error": err.Error(),
-			})
+			ctx.AbortWithStatusJSON(makeMiddlewareResponse(apperrors.ErrHeaderNotProvided.Error()))
 			return
 		}
 
 		fields := strings.Fields(authorizationHeader)
 		if len(fields) < 2 {
-			err := errors.New("invalid authorization header format")
-			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
-				"error": err.Error(),
-			})
+			ctx.AbortWithStatusJSON(makeMiddlewareResponse(apperrors.ErrInvalidHeaderFormat.Error()))
 			return
 		}
 
 		authorizationType := strings.ToLower(fields[0])
 		if authorizationType != authorizationTypeBearer {
-			err := fmt.Errorf("unsupported authorization type %s", authorizationType)
-			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
-				"error": err.Error(),
-			})
+			ctx.AbortWithStatusJSON(makeMiddlewareResponse(apperrors.ErrUnsupportedAuthType.Error()))
 			return
 		}
 
 		accessToken := fields[1]
 		payload, err := jwtService.VerifyToken(accessToken)
 		if err != nil {
-			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
-				"error": err.Error(),
-			})
+			ctx.AbortWithStatusJSON(makeMiddlewareResponse(err.Error()))
 			return
 		}
 
 		ctx.Set(authorizationPayloadKey, payload)
 		ctx.Set(AuthorizationUserIdKey, payload.UserID)
 		ctx.Next()
+	}
+}
+
+type middlewareResponse struct {
+	Status     string `json:"status"`
+	StatusCode int    `json:"status_code"`
+	Message    string `json:"message"`
+	Result     any    `json:"result"`
+}
+
+func makeMiddlewareResponse(message string) (int, middlewareResponse) {
+	return http.StatusUnauthorized, middlewareResponse{
+		Status:     http.StatusText(http.StatusUnauthorized),
+		StatusCode: http.StatusUnauthorized,
+		Message:    message,
+		Result:     nil,
 	}
 }
